b38ad4b8d037135277248c19bcf7709872b9b5bd
JairamB <b.jairam0512@gmail.com>
2021-08-28 17:49:15 +0530

    trial and error commit #1

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:root@localhost:5433/quotesdb"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:root@localhost:5432/quotesdb"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
